ac3b91eebd052842b901a6bc74577556c5e70dc8
Stanisław Synowiec <stasyn1410@gmail.com>
2023-03-04 20:56:59 +0100

    First project

---
+++ .history/src/pages/home-page/scripts/home-page_20230216081117.js
@@ -0,0 +1,8 @@
+/// Homepage JavaScript File
+/// Here we import all the JavaScript files we need for our homepage.
+
+import '../styles/home-page.scss';
+
+
+
+setupCounter(document.querySelector('#counter'));
